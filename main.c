47c179914703b8172d3ead11c733f760b9fd5de6
jonl112 <lewkewarm@outlook.com>
2017-09-23 17:24:35 -0700

    formatting

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main (void)
 {
-printf("Hello World");
-printf("new line");
-printf("master branch change");
-return 0;
+  printf("Hello World");
+  printf("new line");
+  printf("master branch change");
+  return 0;
 }
